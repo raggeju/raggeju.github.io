49ffc3f7475440d466b7aec42e353e7b4cf7a38e
Ragnar Axberg <ragnar.axberg@refapp.com>
2024-04-10 13:35:24 +0200

    Custom elements yä

--- assets/create-element.js
+++ assets/create-element.js
@@ -1,7 +1,8 @@
-function cewa(component, attributes) {
+const cewa = (component, attributes) => {
   const element = document.createElement(component);
+  if (!attributes) return element;
   Object.entries(attributes).forEach(([key, value]) =>
     element.setAttribute(key, value)
   );
   return element;
-}
+};
