66e58d7c9e0c4091f9e7c655e35c82de4a3a0add
Ragnar Axberg <ragnar.axberg@refapp.com>
2025-05-07 10:51:53 +0200

    asdf

--- assets/src/data/bookmarks.js
+++
@@ -1,5 +0,0 @@
-const bookmarks = [
-  "https://birchtree.me/blog/retina-is-not-a-static-ppi/",
-  "https://asdfadsfasdfasdf.se",
-  "https://russmaxdesign.github.io/accessible-error-fieldset/",
-];
--- assets/src/data/my-info.js
+++ assets/src/data/my-info.js
@@ -1,4 +1,4 @@
-const myInfo = {
+const ragnar = {
   names: ["Nils", "Ragnar", "Mauritz", "Axberg", "Svensson"],
   gender: "male",
   pronouns: "he/him/his",
@@ -23,3 +23,104 @@ const myInfo = {
     "Scrum Master",
   ],
 };
+
+const educations = [
+  {
+    course: "Specialization in Film History 7,5 hp",
+    school: "Gothenburg University",
+    from: "2018",
+    to: "2018",
+  },
+  { course: "Game Analysis 7,5 hp", school: "Skövde University", from: "2011", to: "2011" },
+  { course: "Film Studies 30 hp", school: "Gothenburg University", from: "2011", to: "2011" },
+  { course: "Web Developer (2 year polytechnic)", school: "Yrgo", from: "2007", to: "2009" },
+  {
+    course: "Social Science & Media (3 year high school)",
+    school: "Munkebäcksgymnasiet High School",
+    from: "2004",
+    to: "2007",
+  },
+];
+
+const workplaces = [
+  {
+    position: "Front End - / System Developer",
+    employer: "Refapp / Talentwise",
+    from: "September 2022",
+    to: "Present",
+    about:
+      "Refapp is a **system for digital referencing** and professional evaluations. It was built using modern technologies and used by thousands of companies all over the world.",
+    technologies: ["React", "TypeScript", "Meteor", "MUI", "Mocha", "MongoDB", "Figma", "macOS"],
+  },
+  {
+    position: "Senior Front End Developer / UX / Scrum Master",
+    client: "SKF",
+    from: "June 2021",
+    to: "August 2022",
+    about: "Worked on a **UI component library** and related UX / design.",
+    technologies: [
+      "React",
+      "TypeScript",
+      "Styled Components ",
+      "Storybook",
+      "Jest",
+      "Chromatic",
+      "Style Dictionary",
+      "Figma",
+      "macOS",
+    ],
+  },
+  {
+    position: "Senior Front End Developer / Scrum Master",
+    client: "CEVT",
+    from: "January 2019",
+    to: "May 2021",
+    about:
+      "Worked in the connected car software department on **test portal** and **information screens**.",
+    technologies: ["Vue", "React", "Websockets", "Adobe Illustrator + XD", "Windows"],
+  },
+  {
+    position: "Senior Front End Consultant",
+    employer: "Retroid Interactive / Bitwave Games",
+    from: "January 2019",
+    to: "August 2022",
+    about:
+      "Re-joined Retroid as a member of their consultant team. Will focus on assignments within Front End and Web Development.",
+  },
+  {
+    position: "Front End Developer / UX Developer",
+    employer: "Textalk",
+    from: "May 2016",
+    to: "January 2019",
+    about:
+      "Front End Developer and Project Manager for Ortstidningar i väst and their **websites**. UX Designer and Fullstack Developer for the Textalk **webshop plattform** (also known as Abicart).",
+    technologies: [
+      "AngularJS",
+      "Node.js",
+      "PHP",
+      "Sketch",
+      "Photoshop + XD + Premiere + After Effects",
+      "macOS",
+    ],
+  },
+  {
+    position: "Front End Developer / UX Developer",
+    employer: "EWO",
+    from: "April 2015",
+    to: "December 2015",
+    about:
+      "Developed **campaign websites** for ELS Campaign, **bonus club websites** for HD and Sydsvenskan, **food ordering websites** for LetUsEat and**work tracking apps** for CheckEasy and TrackEasy.",
+    technologies: [
+      "AngularJS",
+      "PHP",
+      "Ruby (sinatra)",
+      "Ionic",
+      "Cordova",
+      "Shell-script",
+      "Less",
+      "Sass",
+      "Photoshop + Illustrator",
+      "SQLLite",
+    ],
+  },
+];
